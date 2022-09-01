dfa5aa3d301f1fad4f1579d859bccdb4dad1326a
JamesJPorter <109930071+JamesJPorter@users.noreply.github.com>
2022-09-01 00:50:55 -0700

    created functions to prompt and store user criteria for password generator

--- Develop/script.js
+++ Develop/script.js
@@ -3,12 +3,39 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+  // var password = generatePassword()
 }
+var passwordText = document.querySelector("#password");
+
+passwordText.value = password;
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+// Function to select password length
+var selectPwdLength = function() {
+  pwdLength = window.prompt("What length would you like your password from 8 - 128 characters?");
+  if (pwdLength > 7 && pwdLength < 128) {
+    console.log("User select Length: " + pwdLength)
+    window.alert("User select Length: " + pwdLength)
+  } else selectPwdLength();
+}
+// Function to select uppercase letters in password
+var selectPwdUpperCase = function() {
+  pwdUpperCase = window.prompt("Would you like your password ot be have uppercase letters? Yes or no")
+  if (pwdUpperCase == "yes" || pwdUpperCase == "no") {
+    console.log("User select Uppercase: " + pwdUpperCase)
+    window.alert("User select Uppercase: " + pwdUpperCase);
+  } else (selectPwdUpperCase())
+}
+
+var selectPwdLowerCase = function () {
+  pwdLowerCase = window.prompt("Would you like your password to have lowercase letters? yes or no")
+  if (pwdLowerCase == "yes" || pwdLowerCase == "no")
+  console.log("User select lowercase: " + pwdLowerCase)
+  window.alert("User select lowercase: " + pwdLowerCase);
+}
+
+selectPwdLowerCase();
